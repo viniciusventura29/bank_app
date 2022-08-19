6d2014c03dda0605286803839193246be52c87e2
viniciusventura29 <viniciusventura29@gmail.com>
2022-08-19 11:30:57 -0300

    hero responsive and improving navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -26,23 +26,15 @@ export default function Navbar() {
                 </div>
 
                 <div className="-mx-4 lg:flex lg:items-center">
-                    <a href="#" className="block mx-4 mt-2 text-sm text-gray-700 capitalize lg:mt-0 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400">Web developers</a>
-                    <a href="#" className="block mx-4 mt-2 text-sm text-gray-700 capitalize lg:mt-0 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400">Web Designers</a>
-                    <a href="#" className="block mx-4 mt-2 text-sm text-gray-700 capitalize lg:mt-0 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400">UI/UX Designers</a>
-                    <a href="#" className="block mx-4 mt-2 text-sm text-gray-700 capitalize lg:mt-0 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400">Contact</a>
+                    <a href="#" className="block mx-4 mt-2 text-sm text-gray-700 capitalize lg:mt-0 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400">Pra você</a>
+                    <a href="#" className="block mx-4 mt-2 text-sm text-gray-700 capitalize lg:mt-0 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400">Para seu negócio</a>
+                    <a href="#" className="block mx-4 mt-2 text-sm text-gray-700 capitalize lg:mt-0 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400">Sobre nós</a>
+                    <a href="#" className="block mx-4 mt-2 text-sm text-gray-700 capitalize lg:mt-0 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400">Dúvidas</a>
                 </div>
             </div>
         </div>
     </nav>
-{/* 
-    <div className="w-full bg-center bg-cover h-[32rem]">
-        <div className="flex items-center justify-center w-full h-full bg-gray-900 bg-opacity-50">
-            <div className="text-center">
-                <h1 className="text-2xl font-semibold text-white uppercase lg:text-3xl">Build Your new <span className="text-blue-400 underline">Saas</span></h1>
-                <button className="w-full px-4 py-2 mt-4 text-sm font-medium text-white uppercase transition-colors duration-200 transform bg-blue-600 rounded-md lg:w-auto hover:bg-blue-500 focus:outline-none focus:bg-blue-500">Start project</button>
-            </div>
-        </div>
-    </div> */}
+
 </header>
 
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,41 @@
-import '../styles/App.css'
-import Navbar from '../components/Navbar'
+import "../styles/App.css";
+import Navbar from "../components/Navbar";
+import imagee from "../img/Teste.jpg";
 
 function App() {
   return (
-    <Navbar />
-  )
+    <>
+      <Navbar />
+
+      <div className="w-full bg-center bg-cover h-[32rem]">
+        <div
+          className="flex items-center justify-center w-full h-full"
+          style={{
+            backgroundImage: `url(${imagee})`,           
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+          }}
+        >
+          <div className="text-center grid grid-cols-1 ">
+            <h1 className="text-3xl font-semibold text-white uppercase lg:text-6xl ">
+              lançou lorem local{" "}
+              <span className="text-blue-400">passagem</span>
+            </h1>
+            <span className="text-white px-8 pt-2">
+              Lorem Ipsum é simplesmente uma simulação de texto da indústria
+              tipográfica e de impressos, e vem sendo utilizado desde o século
+              XVI
+            </span>
+            <button className="justify-self-center block px-4 py-2 mt-4 text-sm font-medium text-white uppercase transition-colors duration-200 transform bg-blue-600 rounded-md lg:w-36 hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
+              Start project
+            </button>
+          </div>
+        </div>
+      </div>
+      
+    </>
+  );
 }
 
-export default App
+export default App;
