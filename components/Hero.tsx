cd5c20d36fd9877a8f775c4aa1fa0fdd3268a2cb
viniciusventura29 <viniciusventura29@gmail.com>
2022-11-16 11:27:17 -0300

    👌 IMPROVE: Css Hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,25 +1,25 @@
-import bgimg from "../img/transferir.svg";
+import bgimg from "../img/front-view-of-people-having-a-meeting-in-the-office.jpg";
 
 export default function Hero() {
     return(
-        <div className="w-full flex flex-col lg:flex-row lg:justify-between justify-center items-center h-screen lg:p-36">
+        <div className="w-full flex flex-col lg:flex-row lg:justify-between justify-center items-center h-screen">
           
-          <div className="flex flex-col lg:w-5/12 w-11/12 items-center lg:items-start text-center lg:text-left ">
+          <div className="flex flex-col rounded-lg lg:w-5/12 w-11/12 items-center lg:items-start text-center lg:text-left z-20 backdrop-blur-md  p-4 lg:p-10 lg:ml-20">
 
-            <h1 className="text-3xl font-semibold text-gray-900 uppercase lg:text-6xl ">
+            <h1 className="text-3xl font-semibold text-gray-200 uppercase lg:text-6xl ">
               lançou lorem <span className="text-blue-400 font-bold">passagem</span>
             </h1>
             
-            <span className="text-gray-800 px-8 pt-2 lg:text-justify text-sm mt-6 mb-6 lg:mb-16 lg:text-base lg:px-0">
+            <span className="lg:text-gray-200 text-gray-800 leading-5 px-8 pt-2 lg:text-justify text-sm mt-6 mb-6 lg:mb-16 lg:text-base lg:px-0 ">
               Lorem Ipsum é simplesmente uma simulação inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.Lorem Ipsum é simplesmente uma simulação 
             </span>
 
             <button className="justify-self-center block px-4 py-2 lg:mt-4 text-sm font-medium text-white uppercase transition-colors duration-200 transform bg-blue-600 rounded-md lg:w-36 hover:bg-blue-500 focus:outline-none focus:bg-blue-500 " title="Start project">
-              Start project
+              Join Us
             </button>
             
           </div>
-          <img className="lg:w-5/12 w-6/12" src={bgimg} alt="" />
+          <img className="absolute brightness-[.4] bg-clip-content pb-[30rem] z-10 w-full" src={bgimg} alt="" />
         </div>
     
     )
