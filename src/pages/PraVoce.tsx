879122a258ecf0b0ed62474764467a1802de40e6
viniciusventura29 <viniciusventura29@gmail.com>
2022-11-18 08:28:31 -0300

    📦 NEW: PraVoce Page

---
+++ src/pages/PraVoce.tsx
@@ -0,0 +1,18 @@
+import Navbar from "../../components/Navbar";
+import CardsHome from "../../components/CardsHome";
+import Footer from "../../components/Footer";
+import Hero from "../../components/Hero";
+
+export default function PraVoce(){
+return(
+    <>
+    <Navbar />
+
+    <div>
+        <h1>Pra Você</h1>
+    </div>
+
+    <Footer />
+    </>
+    
+)}
