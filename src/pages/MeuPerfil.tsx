585bdea09d092bb8276037d5878ff438865f8099
viniciusventura29 <viniciusventura29@gmail.com>
2022-12-02 11:28:08 -0300

    📦 NEW: Perfil page

---
+++ src/pages/MeuPerfil.tsx
@@ -0,0 +1,36 @@
+import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
+
+
+export default function Sobre(){
+return(
+    <>
+    <Navbar />
+
+    <div className="z-10 h-[18rem] w-full flex items-end relative opacity-95 bg-gradient-to-r from-sky-400 to-blue-500">
+        <h1 className="z-20 text-7xl lg:text-9xl opacity-90 -m-3 font-bold text-white">MEU PERFIL</h1>
+    </div>
+    <div className="mt-28 mb-20 flex items-center justify-center flex-col">
+            <p className="text-xl">Meu Perfil</p>
+            <h2 className="text-4xl font-bold ">Lorem ipsum</h2>
+            <p className="mt-8 w-4/6 lg:w-3/6 text-justify">e vem sendo utilizado desde o século
+            XVI, quando um impressor desconhecido pegou uma bandeja de tipos e
+            os embaralhou para fazer um livro de modelos de tipos Lorem Ipsum é simplesmente uma simulação de texto da indústria
+            tipográfica e de impressos, e vem sendo utilizado desde o século
+            XVI, quando um impressor desconhecido pegou uma bandeja de tipos e
+            os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum
+            sobreviveu não só a cinco séculos Lorem Ipsum é simplesmente uma simulação de texto da indústria
+            tipográfica e de impressos, e vem sendo utilizado desde o século
+            XVI, quando um impressor desconhecido pegou uma bandeja de tipos e
+            os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum
+            sobreviveu não só a cinco séculosLorem Ipsum é simplesmente uma simulação de texto da indústria
+            tipográfica e de impressos, e vem sendo utilizado desde o século
+            XVI, quando um impressor desconhecido pegou uma bandeja de tipos e
+            os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum
+            sobreviveu não só a cinco séculos</p>
+      </div>
+
+    <Footer />
+    </>
+    
+)}
